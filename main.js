60ec6f4ecbc55d3d7ff1096d135e4912cb5bab9f
AmiteshOP <88427048+AmiteshOP@users.noreply.github.com>
2022-08-23 15:34:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+song = "";
+
+function preload()
+{
+    song = loadSound("music.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+}
+
+function draw() {
+    Image(video, 0, 0, 600, 500);
+}
+
+function play()
+{
+    song.play();
+}
